02038d081d0cf8e3ae80d5b2f40089ad1e16b2a0
younes-sh <sheikhlaryounes@gmail.com>
2023-09-21 17:24:22 +0200

    Edit procces.env.REACT_APP_URL_API

--- client/src/pages/NestedPage/nestedGallery/Sqaur.jsx
+++ client/src/pages/NestedPage/nestedGallery/Sqaur.jsx
@@ -6,7 +6,7 @@ const Squar = () => {
   const [pageNumber, setPageNumber] = useState(0); // شماره صفحه فعلی
   const itemsPerPage = 24; // تعداد آیتم‌ها در هر صفحه
 
-  var URL = process.env.REACT_APP_API_URL
+  const URL = process.env.REACT_APP_URL_API;
 
   useEffect(() => {
       fetch(`${URL}api/items`)
