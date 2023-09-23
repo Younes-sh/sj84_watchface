83bf485e35685f713c104500414cd42e9fe97a14
younes-sh <sheikhlaryounes@gmail.com>
2023-09-23 23:37:56 +0200

    Update

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 // تنظیمات CORS
 
 app.use(cors({
-  origin: 'https://sj84-watchface.vercel.app/gallery/all', // دامنه‌ی منبع
+  origin: 'https://sj84-watchface.vercel.app', // دامنه‌ی منبع
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // متدهای مجاز
   credentials: true, // اجازه‌ی ارسال کوکی‌ها
 }));
