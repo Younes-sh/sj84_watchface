b0aa1f6a08b35093733f5ba435714eef896986c0
younes-sh <sheikhlaryounes@gmail.com>
2023-10-15 17:16:47 +0200

    Create Header and Designe

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import './style.css';
+import Classic from '../../assets/Head/classic.png';
+import Buda from '../../assets/Head/Buda.png';
+import Hack from '../../assets/Head/Hack.png';
+import Roma from '../../assets/Head/Roma.png';
+import Analog from '../../assets/Head/analog.png';
+// Icon Watch
+const Samsung = 'https://img.icons8.com/color/96/samsung.png';
+const Apple = 'https://img.icons8.com/3d-fluency/94/mac-os.png';
+const  WearOs = 'https://cdn1.iconfinder.com/data/icons/logos-brands-in-colors/470/Wear_OS_icon-512.png';
 
 const Header = () => {
   return (
-    <div className='w-100 '>
-        <h1>Header</h1>
-        <div className='w-100 h-100'>
-            
+    <div className='w-100 h-auto p-5  bg-dark text-light d-flex flex-column justify-content-center'>
+        <h1 className='  mt-lg-5'>We have put together the <span className='text-warning'>Best Collection</span></h1>
+        <h1>of watches in the world</h1>
+        
+
+        <div className='w-100 h-100 mt-5 d-flex  justify-content-center'>
+            <span className='px-5'><img   src={Samsung} alt="Samsung" /></span>
+            <span className='px-5'><img className='w-75' src={Apple} alt="Apple" /></span>
+            <span className='px-5'><img style={{width:'70px'}} src={WearOs} alt="wearos" /></span>
         </div>
-        <div className='w-100 h-100'>
-            
+        <div className='Container-watch'>
+          <img className='Buda  ' src={Buda} alt="Buda" />
+          <img className='Hack ' src={Hack} alt="Hack" />
+          <img className='Classic ' src={Classic} alt="Classic" />
+          <img className='Roma ' src={Roma} alt="Roma" />
+          <img className='Analog ' src={Analog} alt="Analog" />
         </div>
+        <br />
     </div>
   )
 }
